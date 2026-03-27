6ca7c2275ba29358fbd24d14f64cca9cb7299822
amoghap17-gif <amoghap17@gmail.com>
2026-03-27 10:36:12 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Gradle Jenk Demo Project!");
+        System.out.println("Hello from Gradle Jeniiik Demo Project!");
     }
 }
